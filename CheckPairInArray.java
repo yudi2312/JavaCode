7c65331b26eb7104eacb2b9558ffe6d52310f585
yudi2312 <udit23@gmail.com>
2018-01-24 13:20:07 +0530

    Create CheckPairInArray.java

---
+++ CheckPairInArray.java
@@ -0,0 +1,21 @@
+public class HelloWorld{
+
+    private static final int MAX = 100000;
+    
+    public static boolean isPresent(int[] arr, int sum){
+        boolean[] binmap = new boolean[MAX];
+        for (int i=0;i<arr.length;i++){
+            int temp = sum - arr[i];
+            if (temp >= 0 && binmap[temp]) {
+                return true;
+            }
+            binmap[arr[i]] = true;
+        }
+        return false;
+    }
+    public static void main(String []args){
+        int A[] = {1, 4, 4, -1, 6};
+        int n = 8;
+        System.out.println(isPresent(A, n));
+    }
+}
